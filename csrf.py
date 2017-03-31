40d4480f7020edaf1822760838b1588a383423b6
Raimon Esteve <raimon@nan-tic.com>
2017-03-31 16:05:41 +0200

    Rename CsrfProtect will be deprecated

--- csrf.py
+++ csrf.py
@@ -1,9 +1,9 @@
 #This file is part galatea blueprint for Flask.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from flask import current_app
-from flask_wtf.csrf import CsrfProtect
+from flask_wtf.csrf import CSRFProtect
 
 with current_app.app_context():
-    csrf = CsrfProtect()
+    csrf = CSRFProtect()
     csrf.init_app(current_app)
